8a06e9b9de178218d9baff84607f0a6dbddc6734
g00056871 <g00056871@aus.edu>
2014-04-18 20:25:13 +0400

    initial files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+   To change this license header, choose License Headers in Project Properties.
+   To change this template file, choose Tools | Templates
+   and open the template in the editor.
+   -->
+<html>
+   <head>
+      <title>SMILE and Assessment wiki integration</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+      <form method="POST" action="main.php">
+         <div align="center">
+            <div> <b>Integrating SMILE questions into the editorial cycle of Assessment Wiki </b></div>
+            <br />
+            Username: <input type="text" name="username" size="15" /><br />
+            Password: <input type="password" name="passwort" size="15" /><br />
+            <a href="newUser.php">Create account</a>
+            <p><input type="submit" value="Login" /></p>
+         </div>
+      </form>
+   </body>
+</html>
---
+++ main.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>SMILE and Assessment wiki integration</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+      <div align="center">
+         <div>
+            <div> <b>Integrating SMILE questions into the editorial cycle of Assessment Wiki </b></div>
+            <br />
+            <input type="button" id="fetchSMILE" value="Fetch SMILE questions"/>&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="button" id="pushAssess" value="Push questions to Assessment wiki"/>
+         </div>
+      </div>
+   </body>
+</html>
